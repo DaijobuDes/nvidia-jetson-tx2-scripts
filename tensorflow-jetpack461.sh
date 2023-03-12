d27a03c8b390ed148537822a6196187482788069
Kate Aubrey Cellan <cellankateaubrey@gmail.com>
2023-03-12 14:50:40 +0800

    Update tensorflow-jetpack461.sh
    Added specific versions since the NVIDIA documentation was updated for newer systems, this script is intended for outdated systems.

--- tensorflow-jetpack461.sh
+++ tensorflow-jetpack461.sh
@@ -24,7 +24,7 @@ apt-get install python3-pip
 pip3 install -U pip testresources setuptools==49.6.0
 
 # Install the Python package dependencies
-pip3 install -U --no-deps numpy==1.19.4 future==0.18.2 mock==3.0.5 keras_preprocessing==1.1.2 keras_applications==1.0.8 gast==0.4.0 protobuf pybind11 cython pkgconfig
+pip3 install -U --no-deps numpy==1.19.4 future==0.18.2 mock==3.0.5 keras_preprocessing==1.1.2 keras_applications==1.0.8 gast==0.4.0 protobuf==3.13.0 pybind11==2.10.0 cython==0.29.32 pkgconfig==1.5.5
 export H5PY_SETUP_REQUIRES=0
 pip3 install -U h5py==3.1.0
 
